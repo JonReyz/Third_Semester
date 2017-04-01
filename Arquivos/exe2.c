a2fee2c7bc3540b75fa95a3b4bcf0a39c5164836
JonReis <joaoreyz@gmail.com>
2017-04-01 10:54:33 -0300

    Termino do trabalho da ciferri

--- Arquivos/exe2.c
+++ Arquivos/exe2.c
@@ -1,9 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-
-#include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 
 typedef struct  PESSOA{
@@ -40,7 +36,7 @@ int main(){
 
 	int size = 16;
 	int quatro = 4;
-
+	int i;
 	int op;
 	int iexit = 1;
 
@@ -63,24 +59,29 @@ int main(){
 		Pessoa *people = malloc(sizeof(Pessoa));
 
 		while(iexit == 1){
-			
+			 size = 16;
 			 printf("Digite o nome do cadasto :\n");
 			 people->nome = readline(&nName);
 		 	 people->n = nName;
 			 size+=nName;
+			// printf("\nTamanho do %d\n",nName);
 			 
 			 printf("Digite o endereço do cadasto :\n");
 			 people->rua = readline(&nRua);
 			 people->r = nRua;
 			 size+=nRua;
-			 
+			// printf("\nTamanho da rua%d\n",nRua);
+
 			 printf("Digite o complemento :\n");
 			 people->complemento = readline(&nComp);
 			 people->c = nComp;
 			 size+=nComp;
+			 //printf("Tamanho do %d\n",nComp);
+
 			 printf("Digite o numero :\n");
 			 scanf("%d",&(people->num));
 			 size+=4;
+
 			 
 			 fwrite(&size,sizeof(int),1,fp);
 			 fwrite(&nName,sizeof(int),1,fp);
@@ -110,36 +111,46 @@ int main(){
 		int tam = ftell(fp2);
 		int nReg;
 		int jump;
-		int i;
 		fseek(fp2,0, SEEK_SET);
 		byte *bytes = malloc(sizeof(byte)*tam);
 		fread(bytes,sizeof(byte),tam,fp2); //peguei todos os dados e armazenei em bytes;
 		byte *aux = bytes; // crio um ponteiro auxiliar
-		nReg = *((int*)aux);
-		//printf("%d",nReg);
-		for(i=0;i<(tam/(nReg +4));i++){
-			aux+=4;
-			jump = *((int*)aux);
-			aux+=4;
+		nReg = *((int*)aux);//pego o tamanho do registro inicial
+		
+		for(i=0;i<tam;){
+			printf("%d",*((int*)aux));
+			aux+=4; //pulo 4 bytes no ponteiro para ignorar o tamanho do registro
+			i+=4;   // faço a mesma coisa com o contador para ter noção de onde estou no vetor de bytes
+			jump = *((int*)aux);// a variavel jump recebe o tamanho do primeiro campo
+
+			aux+=4; //pulo os bytes que representavam o tamanhou do campo
+			i+=4;   // idem com o contador
 			printf("\n Nome : %s ",aux);
-			aux += jump;
-
-			jump = *((int*)aux);
-			aux+=4;
+			aux += jump; // recebo o tamanho do campo para realizar um pulo e chegar no proximo indicador de campo
+			i+=jump;    // idem com o contador
+			jump = *((int*)aux);// recebo o tamanho do próximo campo
+			
+			aux+=4; // pulo os 4 bytes do tamanho do campo
+			i+=4;
 			printf("Endereço: %s ",aux);
-			aux += jump;
-
+			aux += jump; //pulo o tamanho total do campo
+			i+=jump;
+			
 			aux += 4;
+			i+=4;
 			printf("Numero: %d ",*((int*)aux));
-			aux += 4;
+			aux += 4;//pulo os 4 bytes do int
+			i+=4;
 			
 			jump = *((int*)aux);
 			aux+=4;
+			i+=4;
 			printf("Complemento :%s\n",aux);
 			aux += jump;
+			i+=jump;
 		}
+		free(bytes);
 		fclose(fp2);
-		//free(byte);
 	}
 		
 	if(op == 3 ){
@@ -157,15 +168,27 @@ int main(){
 		byte *bytes2 = malloc(sizeof(byte)*tam2);
 		fread(bytes2,sizeof(byte),tam2,fp3); //peguei todos os dados e armazenei em bytes;
 		byte *aux2 = bytes2; // crio um ponteiro auxiliar
-		nReg2 = *((int*)aux2);
 		int rrn;
+		
 		printf("Digite o RRN Desejado :\n");
 		scanf("%d", &rrn);
-		//printf("%d",nReg);
-		//for(i2=0;i2<(tam/(nReg2 +4));i2++){
-			aux2+= rrn * (nReg2+4);
+		i2 = 0;
+		i = 0;
+		while(i2<rrn){ // esse laço vai contando os registros encontrados até achar o desejado
+			if(i<tam2){ // se o contador ainda estiver dentro dos limites do tamanho de arquivo
+				nReg2 = *((int*)aux2); // recebo o tamanho do registro
+				//printf("%d ",nReg2);
+				aux2+=nReg2 + 4; // faço o ponteiro pular o tamanho do registro para chegar no próximo
+				i+= nReg2 + 4; // idem com o contador, para ter o controle de onde estamos no arquivo
+			}		
+			i2++;	
+		}
 
-			aux2+=4;
+		if(i>=tam2) { // caso o usuario digite um rrn inexistente
+			printf("Tamanho de rrn não existente:");
+			return -1;
+		}
+			aux2+=4;   //realizo a mesma lógica da parte 2
 			jump2 = *((int*)aux2);
 			aux2+=4;
 			printf("\n Nome : %s ",aux2);
@@ -184,9 +207,8 @@ int main(){
 			aux2+=4;
 			printf("Complemento :%s\n",aux2);
 			aux2 += jump2;
-		//}
+		free(bytes2);
 		fclose(fp3);
-		//free(byte);
 
 	}
 
